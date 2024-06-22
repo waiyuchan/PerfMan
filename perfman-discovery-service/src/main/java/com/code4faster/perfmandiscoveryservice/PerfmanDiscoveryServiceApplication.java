2affc0af95ccf69d51ed6215032945429edbf570
WaiyuChan <waiyuchan3011@gmail.com>
2024-06-22 15:27:26 +0800

    Completed the service discovery service based on Spring Cloud Eureka

--- perfman-discovery-service/src/main/java/com/code4faster/perfmandiscoveryservice/PerfmanDiscoveryServiceApplication.java
+++ perfman-discovery-service/src/main/java/com/code4faster/perfmandiscoveryservice/PerfmanDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.code4faster.perfmandiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PerfmanDiscoveryServiceApplication {
 
     public static void main(String[] args) {
