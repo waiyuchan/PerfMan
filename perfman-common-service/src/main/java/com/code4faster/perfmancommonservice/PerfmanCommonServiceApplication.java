478e1ea98c1f3075a206f525f70829249d2d5cd3
WaiyuChan <waiyuchan3011@gmail.com>
2024-07-03 02:23:46 +0800

    Updated maven reply.

--- perfman-common-service/src/main/java/com/code4faster/perfmancommonservice/PerfmanCommonServiceApplication.java
+++ perfman-common-service/src/main/java/com/code4faster/perfmancommonservice/PerfmanCommonServiceApplication.java
@@ -3,11 +3,11 @@ package com.code4faster.perfmancommonservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import net.devh.boot.grpc.server.autoconfigure.EnableGrpcServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableGrpcServer
+@EnableEurekaClient
 public class PerfmanCommonServiceApplication {
 
     public static void main(String[] args) {
